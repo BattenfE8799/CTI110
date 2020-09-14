5d1a9f60947a514ee82e0de8723115edd8434174
Battenfe8799 <71232235+Battenfe8799@users.noreply.github.com>
2020-09-13 23:17:36 -0400

    Add files via upload
    first submission

---
+++ P1HW2_BasicMath_ BattenfieldElizabeth.py
@@ -0,0 +1,16 @@
+#A program that does some basic math on two numbers that are entered.
+#September 13, 2020
+#CTI-110 P1HW2 - Basic Math
+#Elizabeth Battenfield
+#
+print("Enter a number:")
+first_num=int(input())
+print("Enter another number:")
+second_num=int(input())
+addit=first_num + second_num
+multiplify=first_num * second_num
+print("First Number Entered:",first_num) #Shows what the first input was that was saved as a variable.
+print("Second Number Entered:",second_num) #shows what the second input was that was saved as a variable.
+print("Sum of both numbers:",addit) #shows the the sum of the inputed numbers that was saved as a variable.
+print("Result of Multiplying the two numbers:",multiplify) #Shows the result of the multiplication of the two inputed numbers that was saved as a variable.
+
